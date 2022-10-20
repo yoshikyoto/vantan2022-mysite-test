213c1f45da695fdc01083fdbf8488738e14bd474
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-10-21 07:53:25 +0900

    アカウント登録

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail', views.detail, name='detail'),
+    path("register", views.AccountCreateView.as_view(), name="register")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.models import User
  
 def index(request):
     return render(request, "blog/index.html")
 
 def detail(request):
     return HttpResponse("detail page")
+
+
+class AccountCreateView(View):
+    def get(self, request):
+        return render(request, "blog/register.html")
+
+    def post(self, request):
+        # ユーザー情報を保存する
+        User.objects.create_user(
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        # 登録完了画面を表示する
+        return render(request, "blog/register_success.html")
