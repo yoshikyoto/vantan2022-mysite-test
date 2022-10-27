60672e35264561b4e7eaacb80abb8458f7766920
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-10-28 02:14:08 +0900

    マイグレション

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- mysite/settings.py
+++ mysite/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
         'PASSWORD': 'mysitedbpassword',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
